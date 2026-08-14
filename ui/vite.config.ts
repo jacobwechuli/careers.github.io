dddee830b0bd2c9acf01e58c511c25f8d1184653
jacobwechuli <wechulijac@gmail.com>
2026-08-14 22:53:11 +0300

    fixed ui profile

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "careersgithubio-production.up.railway.app",
+      "/api": "https://careersgithubio-production.up.railway.app",
     },
   },
 });
